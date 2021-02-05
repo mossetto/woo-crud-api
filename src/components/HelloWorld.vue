17bd283087501dfbc2a8430b0932e00c1e9ba423
javierM <javiermossetto@gmail.com>
2021-02-05 16:26:53 -0300

    Primer Master
    Funcionando listar, editar y crear.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,323 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-data-table
+    :headers="headers"
+    :items="productos"
+    sort-by="regular_price"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Mi WooCommerce</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Nuevo producto
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Título"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.short_description"
+                      label="Descripción corta"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.regular_price"
+                      label="Precio"
+                    ></v-text-field>
+                  </v-col>
+                  <!-- <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.status"
+                      label="Estado"
+                    ></v-text-field>
+                  </v-col> -->
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.id"
+                      label="ID"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        { text: 'ID', value: 'id' },
+        {
+          text: 'Título de producto',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Descripción corta', value: 'short_description' },
+        { text: 'Precio', value: 'regular_price' },
+        { text: 'Estado', value: 'status' },
+        { text: 'Acción', value: 'actions', sortable: false },
+      ],
+      productos: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        regular_price: 0,
+        // status: '',
+        short_description: '',
+        id: 0,
+      },
+      defaultItem: {
+        name: '',
+        regular_price: 0,
+        status: 0,
+        short_description: '',
+        id: 0,
+      },
+    }),
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'Nuevo producto' : 'Editar producto'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+
+          const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+          const api = new WooCommerceRestApi({
+            url: "https://nuevo-eshopping.com/",
+            consumerKey: "ck_39216abb4abf27b0177ae5ec431ed900c10f637b",
+            consumerSecret: "cs_094b361782eb9d1a6189b8d01007df375a95e1ef",
+            version: "wc/v3"
+          });
+
+          // List products
+          api.get("products", {
+            per_page: 20, // 20 products per page
+          })
+            .then((response) => {
+              // Successful request
+              this.productos = response.data
+            })
+
+      },
+
+      editItem (item) {
+        this.editedIndex = this.productos.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.productos.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.productos.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+
+
+          Object.assign(this.productos[this.editedIndex], this.editedItem)
+          // editar
+
+          const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+          const api = new WooCommerceRestApi({
+            url: "https://nuevo-eshopping.com/",
+            consumerKey: "ck_39216abb4abf27b0177ae5ec431ed900c10f637b",
+            consumerSecret: "cs_094b361782eb9d1a6189b8d01007df375a95e1ef",
+            version: "wc/v3"
+          });
+
+          // Edit a product
+          api.put("products/"+this.editedItem.id, {
+              name: this.editedItem.name,
+              short_description: this.editedItem.short_description,
+              regular_price: this.editedItem.regular_price
+          })
+            .then((response) => {
+
+              console.log("Response Data:", response.data);
+            })
+            .catch((error) => {
+              console.log("Response Data:", error.response.data);
+            })
+
+        } else {
+
+          // Create a product
+          this.productos.push(this.editedItem)
+
+          const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+          const api = new WooCommerceRestApi({
+            url: "https://nuevo-eshopping.com/",
+            consumerKey: "ck_39216abb4abf27b0177ae5ec431ed900c10f637b",
+            consumerSecret: "cs_094b361782eb9d1a6189b8d01007df375a95e1ef",
+            version: "wc/v3"
+          });
+
+            api.post("products", {
+              
+              name: this.editedItem.name,
+              short_description: this.editedItem.short_description,
+              regular_price: this.editedItem.regular_price
+            })
+              .then((response) => {
+                console.log("Response Data:", response.data);
+              })
+              .catch((error) => {
+                console.log("Response Data:", error.response.data);
+              })
+
+        }
+        this.close()
+      },
+    },
+  }
+</script>
